a44bd7ad4b309d20c50f332e8eaa691e379b8511
KamilPysz <averagefinlandenjoyersm00th1e@gmail.com>
2024-02-19 10:16:32 +0000

    studentmachine send

---
+++ cpp/liczby_pierwsze/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout<<"Liczby Pierwsze:"<<endl;
+    int l=1;
+    bool LiczbaP;
+    while(true){
+    LiczbaP = true;
+    for(int i=1; i<l;i++){
+    if(l%i==0){
+    LiczbaP = false;
+    }
+}
+if(LiczbaP==true; && l!=1;){
+cout<<l<<endl;}
+l++;
+}
+
+
+
+
+
+
+
+return 0;
+}
