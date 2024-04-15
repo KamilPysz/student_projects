4dab0c5efa3a6da8f2ba32aaa6435975c4aa0240
KamilPysz <averagefinlandenjoyersm00th1e@gmail.com>
2024-04-15 09:19:50 +0000

    studentmachine send

---
+++ python/witajswiecie/main.py
@@ -0,0 +1,16 @@
+print ("witaj swiecie")
+x=5
+y=input("Podaj y")
+print (y)
+
+z=6
+if x > z:
+    print ("x jest mniejsze od z")
+else:
+    print ("x jest wieksze od z")
+
+a=int(input("Podaj A"))
+b=int(input("Podaj B"))
+
+c=a+b
+print (c)
